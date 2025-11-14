2be253f689e44793991b6a1381e8512b0118c4e8
gudenamrutha <106649340+namruthagude@users.noreply.github.com>
2025-11-14 17:27:37 +0530

    Settings

---
+++ GhostGameV6_0/Assets/Scripts/Singletons/AudioManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Singleton;
+
+    [SerializeField]
+    private AudioSource as_background;
+    [SerializeField]
+    private AudioSource as_game;
+    [SerializeField]
+    private AudioSource as_sound;
+
+    [SerializeField]
+    private GameObject go_SettingsHolder;
+    private void Awake()
+    {
+        if(Singleton == null)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayBackgroundMusic()
+    {
+        as_background.Play();
+    }
+
+    public void StopBackgroundMusic()
+    {
+        as_background.Stop();
+    }
+
+    public void PlayGameMusic(AudioClip clip)
+    {
+        as_game.clip = clip;
+
+        as_game.Play();
+
+    }
+
+    public void StopGameMusic()
+    {
+        as_game.clip = null;
+        as_game.Stop();
+    }
+
+    public void PlayOneShotSFX(AudioClip clip)
+    {
+        as_sound.PlayOneShot(clip);
+    }
+
+    public  void OnMusicVolumeChanged(float volume)
+    {
+        as_background.volume = volume;
+        as_game.volume = volume;
+    }
+
+    public void OnSFXVolumeChanged(float vol)
+    {
+        as_sound.volume = vol;
+    }
+
+
+    public void ShowSettings()
+    {
+        go_SettingsHolder.SetActive(true);
+    }
+}
---
+++ GhostGameV6_0/Assets/Scripts/UI/SettingsUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class SettingsUI : MonoBehaviour
+{
+    [SerializedField]
+    private Slider slider_Music;
+    [SerializeField]
+    private Slider slider_sfx;
+    [SerializeField]
+    private TMP_Dropdown dropdown_quality;
+}
+
+
+
+
+
